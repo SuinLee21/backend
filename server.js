93a80972a43e6010e9e6868d6e4f481e0405313d
SuinLee21 <suin7473@naver.com>
2024-02-19 08:45:05 +0000

    로그인, 회원가입, 로그아웃, 회원탈퇴, 아이디 찾기, 비밀번호 찾기 api 작성 초안

--- server.js
+++ server.js
@@ -1,33 +1,88 @@
-const express = require("express"); //express.js를 import 한 것
+const express = require("express");
 
 const app = express();
 const port = 8000;
 
-//우리가 통신에서 json으로 값을 주고 받긴하지만, json은 원래 통신에 사용할 수 없는 자료구조임. json을 string으로 변환해서 보내고,
-//받는 쪽은 이걸 json으로 변환해서 사용함.
 app.use(express.json());
 
-//api (파일을 반환하는 api)
 app.get("/mainpage", (req, res) => {
-    res.sendFile(`${__dirname}/main.html`) //절대 경로 적어야 되는 데 __dirname으로 가능
-})
+    res.sendFile(`${__dirname}/main.html`)
+});
 
-//api (값을 반환하는 api)
 app.post("/login", (req, res) => {
     const { id, pw } = req.body;
     const result = {
         "success": false
-    }// 통신할 때는 obj
+    }
 
-    //데이터베이스 통신
     if (id === "suin" && pw === "suin") {
         result.success = true;
     }
 
-    //값 반환
     res.send(result);
-})
+});
+
+app.post("/signUp", (req, res) => {
+    const { id, pw, name, phoneNum } = req.body;
+    const result = {
+        "success": false
+    }
+
+    //정규식
+    //db에 입력
+
+    res.send(result);
+});
+
+app.get("/logout", (req, res) => {
+    //세션 삭제
+    res.sendFile(`${__dirname}/main.html`)
+});
+
+app.delete("/withdrawal", (req, res) => {
+    //db에서 데이터 삭제
+    res.sendFile(`${__dirname}/main.html`);
+});
+
+app.get("/users/id", (req, res) => {
+    const { name, phoneNum } = req.body;
+    const result = {};
+
+    if (name === "이수인" && phoneNum === "01012345678") {
+        result.id = "suin"
+    }
+
+    res.send(result);
+});
+
+app.post("/users/password", (req, res) => {
+    const { id, name, phoneNum } = req.body;
+    const result = {};
+
+    if (id === "suin" && name === "이수인" && phoneNum === "01012345678") {
+        result.pw = "suin"
+    }
+
+    res.send(result);
+});
 
 app.listen(port, () => {
     console.log(`${port}번에서 HTTP Web Server 실행`);
-}); //web server 실행 코드 //aws에서 8000을 막아서 이거를 풀어줘야 됨.
+});
+
+
+//로그인
+//회원가입
+//로그아웃
+//회원탈퇴
+//아이디 찾기
+//비밀번호 찾기
+//내정보 보기
+//내정보 수정
+//게시글 쓰기
+//게시글 읽기
+//게시글 수정
+//게시글 삭제
+//댓글 쓰기
+//댓글 수정
+//댓글 삭제
