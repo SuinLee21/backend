7189277160a8e04c21e4e9f16c5e165b28c04487
SuinLee21 <suin7473@naver.com>
2024-02-22 08:42:16 +0000

    예외처리 및 api 수정

--- server.js
+++ server.js
@@ -14,15 +14,13 @@ app.use(session({
 }));
 
 app.get("/mainpage", (req, res) => {
-    console.log(req.session.userId);
-    console.log(req.session.userPw);
     res.sendFile(`${__dirname}/main.html`)
 });
 app.get("/login", (req, res) => {
     res.sendFile(`${__dirname}/login.html`)
 });
 app.get("/test", (req, res) => {
-    if (req.session.userId) {
+    if (req.session.idx) {
         res.sendFile(`${__dirname}/test.html`);
     } else {
         res.sendFile(`${__dirname}/test1.html`);
@@ -64,102 +62,111 @@ const checkValidity = (req, res, next) => {
 //로그인
 app.post("/login", checkValidity, (req, res) => {
     const { userId, userPw } = req.body;
-    console.log(userId, userPw)
 
     if (userId === "qwerqwer1" && userPw === "qwerqwer1!") {
-        req.session.userId = userId;
-        req.session.userPw = userPw;
+        req.session.idx = idx; //db에서 찾은 idx
         res.redirect('/mainpage');
     } else {
-        res.sendFile(`${__dirname}/test1.html`);
+        res.redirect('/login');
     }
-
 });
 
 //회원가입
 app.post("/signUp", checkValidity, (req, res) => {
     //db 에 저장
-    res.sendFile(`${__dirname}/main.html`);
+    res.redirect('/mainpage');
 });
 
 //로그아웃
 app.get("/logout", (req, res) => {
-    req.session.destroy();
-    res.redirect('/mainpage');
+    if (req.session.idx) {
+        req.session.destroy();
+        res.redirect('/mainpage');
+    } else {
+        res.redirect('/test1');
+    }
 });
 
 //회원탈퇴 //DELETE users는?
 app.delete("/users/:idx", (req, res) => {
     const idx = req.params.idx;
-    //db에서 데이터 삭제
-    res.sendFile(`${__dirname}/main.html`);
+
+    if (req.session.idx === idx) {
+        //db에서 데이터 삭제
+        res.redirect('/login');
+    } else {
+        res.redirect('/test1');
+    }
 });
 
 //아이디 찾기
 app.get("/users-id", checkValidity, (req, res) => {
-    const { name, phoneNum } = req.body;
+    const { userName, userPhoneNum } = req.body;
     const result = {};
 
-    if (name === "이수인" && phoneNum === "01012345678") {
-        result.id = "suin"
+    if (userName === "이수인" && userPhoneNum === "01012345678") {
+        result.userId = "suin"
     }
 
     res.send(result);
 });
 
+//아이디 찾기 결과
 app.get("/users-id/:idx", (req, res) => {
     const idx = req.params.idx;
-    const result = {};
 
-    res.send(result);
+    res.send(true);
 });
 
 //비밀번호 찾기 //
 app.post("/users-password", (req, res) => {
-    const { id, name, phoneNum } = req.body;
+    const { userId, userName, userPhoneNum } = req.body;
     const result = {};
 
-    if (id === "suin" && name === "이수인" && phoneNum === "01012345678") {
-        result.pw = "suin"
+    if (userId === "suin" && userName === "이수인" && userPhoneNum === "01012345678") {
+        result.userPw = "suin"
     }
 
     res.send(result);
 });
 
+//비밀번호 찾기 결과
 app.get("/users-password/:idx", (req, res) => {
     const idx = req.params.idx;
-    const result = {};
 
-    res.send(result);
+    res.send(true);
 })
 
 //내 정보 보기
 app.get("/users/:idx", (req, res) => {
     const idx = req.params.idx;
     const result = {};
-    //db에서 정보 부르기
 
-    result.id = "suin";
-    result.pw = "suin";
-    result.name = "이수인";
-    result.phoneNum = "01012345678";
+    if (req.session.id === idx) {
+        //db에서 정보 부르기
+        result.userId = "suin";
+        result.userPw = "suin";
+        result.userName = "이수인";
+        result.userPhoneNum = "01012345678";
 
-    res.send(result);
+        res.send(result);
+    } else {
+        res.redirect('/test1');
+    }
 });
 
 //내 정보 수정
 app.put("/users/:idx", checkValidity, (req, res) => {
     const idx = req.params.idx;
-    const { id, pw, name, phoneNum } = req.body;
-    const result = {};
 
-    result.id = "suin";
-    result.pw = "suin";
-    result.name = "이수인";
-    result.phoneNum = "01012345678";
+    if (req.session.idx === idx) {
+        const { userId, userPw, userName, userPhoneNum } = req.body;
+        //db에 넣기
+        res.redirect('/mainpage');
+    } else {
+        res.redirect('/test1');
+    }
 
-    //db에 넣기
-    res.send(result);
 })
 
 //게시글 읽기
@@ -174,77 +181,82 @@ app.get("/post/:idx", (req, res) => {
 
 //게시글 작성
 app.post("/post", (req, res) => {
-    const { title, contents } = req.body;
-    //예외처리
-    //db에 추가
+    if (req.session.idx) {
+        const { title, contents } = req.body;
 
-    res.sendFile(`${__dirname}/main.html`);
+        //db에 추가
+
+        res.redirect('/mainpage');
+    } else {
+        res.redirect('/test1');
+    }
 })
 
 //게시글 수정
 app.put("/post/:idx", (req, res) => {
     const idx = req.params.idx;
-    const { title, contents } = req.body;
 
-    //예외처리
-    //db에 추가
+    if (req.session.idx === idx) {
+        const { title, contents } = req.body;
 
-    res.sendFile(`${__dirname}/main.html`);
+        //db에 추가
+
+        res.redirect('/mainpage');
+    } else {
+        res.redirect('/test1');
+    }
 })
 
 //게시글 삭제
 app.delete("/post/:idx", (req, res) => {
     const idx = req.params.idx;
 
-    //삭제
-    res.sendFile(`${__dirname}/main.html`);
+    if (req.session.idx === idx) {
+        //삭제
+        res.redirect('/mainpage');
+    } else {
+        res.redirect('/test1');
+    }
 })
 
 //댓글 작성
 app.post("/comment", (req, res) => {
-    const { contents } = req.body;
-    //예외처리
-    //db에 추가
+    if (req.session.idx) {
+        const { contents } = req.body;
+        //db에 추가
 
-    res.sendFile(`${__dirname}/main.html`);
+        res.redirect('/mainpage');
+    } else {
+        res.redirect('/test1');
+    }
 })
 
 //댓글 수정
 app.put("/comment/:idx", (req, res) => {
     const idx = req.params.idx;
-    const { contents } = req.body;
 
-    //예외처리
-    //db에 추가
-    res.sendFile(`${__dirname}/main.html`);
+    if (req.session.idx === idx) {
+        const { contents } = req.body;
+
+        //db에 추가
+        res.redirect('/mainpage');
+    } else {
+        res.redirect('/test1');
+    }
 })
 
 //댓글 삭제
 app.delete("/comment/:idx", (req, res) => {
     const idx = req.params.idx;
-    //삭제
-    res.sendFile(`${__dirname}/main.html`);
+
+    if (req.session.idx === idx) {
+        //삭제
+        res.redirect('/mainpage');
+    } else {
+        res.redirect('/test1');
+    }
 })
 
 app.listen(port, () => {
     console.log(`${port}번에서 HTTP Web Server 실행`);
 });
-
-
-//로그인
-//회원가입
-//로그아웃
-//회원탈퇴
-//아이디 찾기
-//비밀번호 찾기
-//내정보 보기
-//내정보 수정
-
-//게시글 쓰기
-//게시글 읽기
-//게시글 수정
-//게시글 삭제
-
-//댓글 쓰기
-//댓글 수정
-//댓글 삭제
