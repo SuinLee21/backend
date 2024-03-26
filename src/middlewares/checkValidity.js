a7e4155e84018b0ee13850415454dd02b15ac62c
SuinLee21 <suin7473@naver.com>
2024-03-26 09:06:48 +0000

    checkValidity.js 수정

--- src/middlewares/checkValidity.js
+++ src/middlewares/checkValidity.js
@@ -4,7 +4,7 @@ const checkValidity = (req, res, next) => {
     const regexName = /^[가-힣]{2,10}$/ //한글만 2~10;
     const regexPhoneNum = /^010-\d{4}-\d{4}$/;
 
-    const { userId, userPw, userName, userPhoneNum } = req.body;
+    const { userId, userPw, userName, userPhoneNum } = Object.keys(req.body).length == 0 ? req.query : req.body;
     const result = {
         "message": ""
     };
@@ -12,26 +12,25 @@ const checkValidity = (req, res, next) => {
     if (userId) {
         if (!regexId.test(userId)) {
             result.message = "아이디를 다시 입력해주세요";
-            console.log('aa')
             return res.status(404).send(result);
         }
     }
     if (userPw) {
         if (!regexPw.test(userPw)) {
             result.message = "비밀번호를 다시 입력해주세요";
-            return res.send(result);
+            return res.status(404).send(result);
         }
     }
     if (userName) {
         if (!regexName.test(userName)) {
             result.message = "이름을 다시 입력해주세요";
-            return res.send(result);
+            return res.status(404).send(result);
         }
     }
     if (userPhoneNum) {
         if (!regexPhoneNum.test(userPhoneNum)) {
             result.message = "전화번호를 다시 입력해주세요";
-            return res.send(result);
+            return res.status(404).send(result);
         }
     }
     next();
